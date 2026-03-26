2132470fdb6708870089f896b1025cca7e9eadd5
vin0dkhichar <abbb61667@gmail.com>
2026-03-26 14:38:53 +0530

    G2P-4396 added actions for messages and version history

--- src/features/messages/utils/incomingMessage.actions.ts
+++ src/features/messages/utils/incomingMessage.actions.ts
@@ -1,8 +1,7 @@
 import { NestedValues } from "@/shared/types/types";
 
 export const INCOMING_MESSAGE_ACTIONS = {
-    read: "incoming_message.read",
-    // list: "incoming_message.list",
+    view: "incomingMessage:view",
 } as const;
 
 export type IncomingMessageAction = NestedValues<typeof INCOMING_MESSAGE_ACTIONS>;
