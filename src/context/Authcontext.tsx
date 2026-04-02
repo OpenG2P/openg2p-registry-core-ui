9d6968bdabc7f3c2d954fcf586fafaf729a3a5ae
vin0dkhichar <abbb61667@gmail.com>
2026-04-02 15:09:07 +0530

    G2P-4471 Fix: Proper message should be show in page for unauthorized access

--- src/context/Authcontext.tsx
+++ src/context/Authcontext.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { createContext, useContext, useState, useEffect, useCallback, ReactNode } from 'react';
-
+import Image from 'next/image';
+import { useTranslations } from 'next-intl';
 
 interface AuthContextType {
     isLoggedIn: boolean;
@@ -17,6 +18,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const [user, setUser] = useState<any | null>(null);
     const [isLoading, setIsLoading] = useState(true);
     const [errorCode, setErrorCode] = useState<string | null>(null);
+    const t = useTranslations();
 
     const logout = useCallback(() => {
         setIsLoggedIn(false);
@@ -61,6 +63,11 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
                 if (res.status === 413) {
                     setErrorCode('G2P-AUT-413');
+                    return;
+                }
+
+                if (res.status === 403) {
+                    setErrorCode('G2P-AUT-403');
                     return;
                 }
 
@@ -169,7 +176,32 @@ export function AuthProvider({ children }: { children: ReactNode }) {
         );
     }
 
-    if (!isLoggedIn) return null;
+    if (errorCode === 'G2P-AUT-403' || !isLoggedIn) {
+        return (
+            <div className="w-full min-h-screen flex items-center justify-center bg-white px-4">
+                <div className="flex flex-1 items-center justify-start">
+                    <div className="w-full bg-white rounded-[10px] flex flex-col items-center text-center">
+                        <Image
+                            src="/images/common/forbidden.png"
+                            width={140}
+                            height={140}
+                            alt="Forbidden illustration"
+                            className="mb-6"
+                            priority
+                        />
+
+                        <h1 className="mb-4 text-[40px] font-semibold leading-11.75 text-[#ED7C22]">
+                            {t('access_denied')}
+                        </h1>
+
+                        <p className="mb-6 text-[20px] font-light leading-6 text-black/50 max-w-xl">
+                            {t('forbidden_subtitle_application')}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, user, logout, handleUnauthorized }}>
